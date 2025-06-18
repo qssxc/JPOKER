67015b2ef5ed754ca4a1fc5f188499ea22150f2a
yu-na-lee <202445007@itc.ac.kr>
2025-06-18 09:42:19 +0900

    Add files via upload

---
+++ dump-jpoker-202506180939.sql
@@ -0,0 +1,83 @@
+-- MySQL dump 10.13  Distrib 8.0.41, for Win64 (x86_64)
+--
+-- Host: localhost    Database: jpoker
+-- ------------------------------------------------------
+-- Server version	8.0.41
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `game_info`
+--
+
+DROP TABLE IF EXISTS `game_info`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `game_info` (
+  `TotalGame` int DEFAULT NULL,
+  `TotalWin` int DEFAULT NULL,
+  `Player_id` varchar(20) DEFAULT NULL,
+  `Money` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `game_info`
+--
+
+LOCK TABLES `game_info` WRITE;
+/*!40000 ALTER TABLE `game_info` DISABLE KEYS */;
+INSERT INTO `game_info` VALUES (14,9,'2025','16700'),(0,0,'2026','0'),(4,2,'2027','10900');
+/*!40000 ALTER TABLE `game_info` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `player_info`
+--
+
+DROP TABLE IF EXISTS `player_info`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `player_info` (
+  `player_id` varchar(20) NOT NULL,
+  `password` varchar(20) DEFAULT NULL,
+  `name` varchar(50) DEFAULT NULL,
+  `email` varchar(100) DEFAULT NULL,
+  `totalgame` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`player_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `player_info`
+--
+
+LOCK TABLES `player_info` WRITE;
+/*!40000 ALTER TABLE `player_info` DISABLE KEYS */;
+INSERT INTO `player_info` VALUES ('','','','',NULL),('2024','9999','홍길동','9999@gmail.com',NULL),('202445000','1234','김길동','gd@gmail.com',NULL),('2025','9999','홍길동','9999@gmail.com',NULL),('2026','3333','최길동','as@gmail',NULL),('2027','6666','송길동','fdsfds',NULL);
+/*!40000 ALTER TABLE `player_info` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping routines for database 'jpoker'
+--
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2025-06-18  9:39:18
